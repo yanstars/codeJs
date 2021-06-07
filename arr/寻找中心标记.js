73bbdf9247b9d151c1b7eb79d62b05c4db64917b
yanxinbo <yanxinbo@51shebao.com>
2021-06-07 23:49:27 +0800

    arr

--- arr/寻找中心标记.js
+++ arr/寻找中心标记.js
@@ -1,1 +1,24 @@
-// [1, 2, 8, 3, 9 ,3]   1+2+8 == 9+3 =》3
+// [1, 2, 8, 3, 9 ,2]   1+2+8 == 9+2=》3
+// 返回中心元素的下标
+
+function findCenterItem (arr) {
+
+  let sum = arr.reduce((pre, cru) => pre + cru)
+
+  for (let i = 0, count = 0, len = arr.length; i < len - 1; i++) {
+    console.log('index :>> ', i)
+
+    if (count == sum - count - arr[i]) {
+      return i
+    }
+    if (count > sum - count - arr[i]) {
+      return -1
+    }
+    count += arr[i]
+  }
+  return -1
+}
+
+
+console.log('findCenterItem :>> ', findCenterItem([1, 1, 1, 3, 9, 2]))
+console.log('findCenterItem :>> ', findCenterItem([1, 2, 8, 3, 9, 2]))
